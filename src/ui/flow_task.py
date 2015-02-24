d4241d3a9dbdb58c6f52b6aebd604e3420c6da63
Brian Teague <bpteague@gmail.com>
2015-02-23 20:11:54 -0500

    Starting to get UI structure.

--- src/ui/flow_task.py
+++ src/ui/flow_task.py
@@ -5,7 +5,8 @@ Created on Feb 11, 2015
 """
 
 from pyface.tasks.api import Task, TaskLayout, PaneItem
-from flow_view_pane import FlowViewPane
+from example_panes import PythonScriptBrowserPane
+from flow_task_pane import FlowTaskPane
 
 class FlowTask(Task):
     """
@@ -15,6 +16,17 @@ class FlowTask(Task):
     id = "edu.mit.synbiocenter.flow_task"
     name = "Flow cytometry analysis pane"
     
+    def _default_layout_default(self):
+        return TaskLayout( left = PaneItem("browser_l"),
+                           right = PaneItem("browser_r"))
+    
     def create_central_pane(self):
-        return FlowViewPane()
+        return FlowTaskPane()
+    
+    def create_dock_panes(self):
+        browser_l = PythonScriptBrowserPane()
+        browser_l.id = "browser_l"
+        browser_r = PythonScriptBrowserPane()
+        browser_r.id = "browser_r"
+        return [browser_l, browser_r]
         
---
+++ src/ui/flow_task_pane.py
@@ -0,0 +1,30 @@
+"""
+Created on Feb 11, 2015
+
+@author: brian
+"""
+from pyface.tasks.task_pane import TaskPane
+from matplotlib_editor import MPLFigureEditor
+from traits.api import Instance, provides
+from pyface.tasks.i_task_pane import ITaskPane
+
+@provides(ITaskPane)
+class FlowTaskPane(TaskPane):
+    """
+    The center pane for the UI; contains the matplotlib canvas for plotting
+    data views.  eventually, this will allow multiple views; for now, it's
+    just one matplotlib canvas.
+    """
+    
+    id = 'edu.mit.synbio.flow_task_pane'
+    name = 'Cytometry Data Viewer'
+    
+    editor = Instance(MPLFigureEditor)
+    
+    def create(self, parent):
+        self.editor = MPLFigureEditor(parent)
+        self.control = self.editor.control
+
+    def destroy(self):
+        self.editor.destroy()
+        self.control = self.editor = None
