0e2db032f96c2df74e32dd0da58c0253c6492b01
Brian Teague <teague@mit.edu>
2015-03-04 19:27:50 -0500

    TraitsUI-based dialog to add new experimental conditions.

--- src/ui/experiment_dialog.py
+++ src/ui/experiment_dialog.py
@@ -11,12 +11,12 @@ ETSConfig.toolkit = 'qt4'
 import os
 os.environ['TRAITS_DEBUG'] = "1"
 
-from traits.api import HasTraits, provides, Instance, Str, Int, Property, List, \
-                       Constant, Color, Bool, on_trait_change, Event
+from traits.api import HasTraits, provides, Instance, Str, Int, List, \
+                       Bool, Enum, Float
 
 from traitsui.api import UI, Group, View, Item, TabularEditor
 from traitsui.tabular_adapter import TabularAdapter
-from traitsui.menu import Menu, Action
+from traitsui.menu import Menu, Action, OKCancelButtons
 from traitsui.handler import Handler, Controller
 from traitsui.qt4.tabular_editor import TabularEditorEvent
 
@@ -39,9 +39,12 @@ class ExperimentSetupAdapter(TabularAdapter):
     # List of (Column labels, Column ID).
     columns = [ ('Name',  'Name')]
     
-    column_menu = Menu(Action(name = "Add...", 
-                              action = 'handler._on_add(object, column, info)',
-                              enabled_when = 'column == 0'))
+    #column_menu = Menu(Action(name = "Add...", 
+    #                          action = 'handler._on_add(object, column, info)',
+    #                          enabled_when = 'column == 0'))
+    
+    column_menu = Menu(Action(name = "Delete",
+                              action = 'handler._on_delete_column(object, column, info)'))
     
 class Tube(HasTraits):
     """
@@ -53,6 +56,36 @@ class Tube(HasTraits):
     
     File = Str
     Name = Str
+    
+    def traits_equal(self, other):
+        """ Are the traits (except File, Name, Row, Col) equal? """
+        
+        for trait in self.trait_names():
+            if trait in ["File", "Name", "Row", "Col"]:
+                continue
+            if not self.trait_get(trait) == other.trait_get(trait): 
+                return False
+            
+        return True
+    
+    '''
+    def __eq__(self, other):
+        """
+        Compare all the traits for equality *except* File, Name, Row and Col
+        """
+        
+        for trait in self.trait_names():
+            if trait in ["File", "Name", "Row", "Col"]:
+                continue
+            if not self.trait_get(trait) == other.trait_get(trait): 
+                return False
+            
+        return True
+        
+    # because we've changed __eq__, have to define __hash__ too
+    def __hash__(self):
+        for trait in self.trait_names():
+    '''
 
 class ExperimentSetup(HasTraits):
     """
@@ -62,9 +95,10 @@ class ExperimentSetup(HasTraits):
     tubes = List(Tube)
 
     # traits to communicate with the TabularEditor
-    col_clicked = Instance('TabularEditorEvent')
+    col_clicked = Instance(TabularEditorEvent)
     update = Bool
     refresh = Bool
+    selected = List
     
     tube_metadata = {}
     
@@ -79,9 +113,11 @@ class ExperimentSetup(HasTraits):
                                         multi_select = True,
                                         auto_update = True,
                                         auto_resize = True,
+                                        #auto_resize_rows = True,
                                         column_clicked = "col_clicked",
                                         update = "update",
-                                        refresh = "refresh")),
+                                        refresh = "refresh",
+                                        selected = "selected")),
             show_labels = False
         ),
         title     = 'Experiment Setup',
@@ -102,12 +138,12 @@ class ExperimentHandler(Controller):
     
     def _on_ok(self):
         print "clicked okay"
-        # TODO - validate input
+        
+        # this is kind of 
         self.dialog.control.accept()
         
-    def _on_add(self, experiment, column, uiinfo):
-        print "add handled"
-        print column
+    def _on_delete_column(self, obj, column, info):
+        pass
         
     def _on_col_clicked(self, click_event): 
         """
@@ -116,6 +152,35 @@ class ExperimentHandler(Controller):
         
         sort_trait = self.model.adapter.columns[click_event.column][1]
         self.model.tubes.sort(key = lambda x: x.trait_get([sort_trait]))
+        
+    def _on_add_condition(self):
+        """
+        Add a new condition.  Use TraitsUI to make a simple dialog box.
+        """
+        
+        class NewTrait(HasTraits):    
+            condition_name = Str
+            condition_type = Enum(["String", "Number", "True/False"])
+    
+            view = View(Item(name = 'condition_name'),
+                        Item(name = 'condition_type'),
+                        buttons = OKCancelButtons,
+                        title = "Add a trait",
+                        close_result = False)
+        
+        new_trait = NewTrait()
+        new_trait.edit_traits(kind = 'modal')
+        
+        if not new_trait.condition_name: 
+            return
+        
+        if new_trait.condition_type == "String":
+            self._add_metadata(new_trait.condition_name, Str)
+        elif new_trait.condition_type == "Number":
+            self._add_metadata(new_trait.condition_name, Float)
+        else:
+            self._add_metadata(new_trait.condition_name, Bool)
+        
         
     def _on_add_tubes(self):
         """
@@ -134,6 +199,7 @@ class ExperimentHandler(Controller):
             tube = Tube(file = path)
             fcs = FCMeasurement(ID='new tube', datafile = path)
             tube.Name = fcs.meta['$SRC']
+            tube.on_trait_change(self._try_multiedit, '+')
             self.model.tubes.append(tube)
     
     def _on_add_plate(self):
@@ -162,7 +228,13 @@ class ExperimentHandler(Controller):
                         Row = well_data.position['new plate'][0],
                         Col = well_data.position['new plate'][1],
                         Name = well_data.meta['$SRC'])
+            tube.on_trait_change(self._try_multiedit, '+')
             self.model.tubes.append(tube)
+            
+    def _try_multiedit(self, obj, name, old, new):
+        print "trait changed"
+        print "selected:"
+        print self.model.selected
         
     def _add_metadata(self, meta_name, meta_type):
         
@@ -239,6 +311,11 @@ class ExperimentSetupDialog(Dialog):
         QtCore.QObject.connect(btn_plate, QtCore.SIGNAL('clicked()'),
                                self.handler._on_add_plate)
         
+        btn_add_cond = QtGui.QPushButton("Add condition...")
+        layout.addWidget(btn_add_cond)
+        QtCore.QObject.connect(btn_add_cond, QtCore.SIGNAL('clicked()'),
+                               self.handler._on_add_condition)
+        
         layout.addStretch()
 
         # 'OK' button.
@@ -246,7 +323,7 @@ class ExperimentSetupDialog(Dialog):
         btn_ok.setDefault(True)
         layout.addWidget(btn_ok)
         QtCore.QObject.connect(btn_ok, QtCore.SIGNAL('clicked()'),
-                               self.handler._on_ok)
+                               self.handler._on_ok)  # so we can do some validation
                                #self.control, QtCore.SLOT('accept()'))
 
         # 'Cancel' button.
@@ -263,7 +340,13 @@ class ExperimentSetupDialog(Dialog):
         
         self.ui = self.model.edit_traits(kind='subpanel', 
                                          parent=parent, 
-                                         handler=self.handler)       
+                                         handler=self.handler)   
+        
+        #tab_control = self.ui.get_editors('tubes')[0].control
+  
+        # turn off row-only selection
+        #tab_control.setSelectionBehavior(QtGui.QAbstractItemView.SelectItems)
+ 
         return self.ui.control
     
     def destroy(self):
@@ -271,6 +354,7 @@ class ExperimentSetupDialog(Dialog):
         self.ui = None
         
         super(Dialog, self).destroy()
+        
     
 if __name__ == '__main__':
 
