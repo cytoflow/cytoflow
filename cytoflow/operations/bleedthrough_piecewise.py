67054dbb1fdfab830d2127af876d5b383b7aaacf
Brian Teague <teague@mit.edu>
2015-08-27 22:37:15 -0400

    Working bleedthrough

--- cytoflow/operations/bleedthrough_piecewise.py
+++ cytoflow/operations/bleedthrough_piecewise.py
@@ -6,12 +6,15 @@ Created on Aug 26, 2015
 
 from __future__ import division
 
-from traits.api import HasStrictTraits, Str, CStr, File, Dict, Python, Instance, \
-                       Int
+from traits.api import HasStrictTraits, Str, CStr, CInt, File, Dict, Python, \
+                       Instance, Int, CFloat
 import numpy as np
 from traits.has_traits import provides
 from cytoflow.operations.i_operation import IOperation
 import FlowCytometryTools as fc
+import math
+import scipy.interpolate
+import scipy.optimize
 
 from ..views import IView
 
@@ -44,12 +47,14 @@ class BleedthroughPiecewiseOp(HasStrictTraits):
         This gets persisted by pickle(); while `controls` and `knots` don't,
         they're to parameterize `estimate()`.
         
+    blank_file : File
+    
     controls : Dict(Str, File)
         The channel names to correct, and corresponding single-color control
         FCS files to estimate the correction splines with.  Must be set to
         use `estimate()`.
         
-    num_knots : Int (default = 5)
+    num_knots : Int (default = 7)
         The number of internal control points to estimate, spaced log-evenly
         from 0 to the range of the channel.  Must be set to use `estimate()`.
     """
@@ -60,11 +65,12 @@ class BleedthroughPiecewiseOp(HasStrictTraits):
     
     name = CStr()
 
-    bleedthrough = Dict(Dict(Str, Python))
-    voltage = Dict(Str, Int)
+    bleedthrough = Dict(Str, Dict(Str, Python))
+    voltage = Dict(Str, CInt)
     
+    blank_file = File
     controls = Dict(Str, File, transient = True)
-    knots = Int(5, transient = True)
+    num_knots = Int(5, transient = True)
     
     def is_valid(self, experiment):
         """Validate this operation against an experiment."""
@@ -94,12 +100,36 @@ class BleedthroughPiecewiseOp(HasStrictTraits):
         """
         Estimate the bleedthrough from the single-channel controls in `controls`
         """
+
+        if self.num_knots < 3:
+            raise RuntimeError("Need to allow at least 3 knots in the spline")
         
         channels = self.controls.keys()
         
         # make sure the voltages are all in order
+        
+        blank_tube = fc.FCMeasurement(ID='blank', datafile = self.blank_file)
+        
+        try:
+            blank_tube.read_meta()
+        except Exception:
+            raise RuntimeError("FCS reader threw an error on tube {0}".format(self.blank_file))
+
+        for channel in channels:
+            exp_v = experiment.metadata[channel]['voltage']
+        
+            if not "$PnV" in blank_tube.channels:
+                raise RuntimeError("Didn't find a voltage for channel {0}" \
+                                   "in tube {1}".format(channel, blank_tube.datafile))
+            
+            control_v = blank_tube.channels[blank_tube.channels['$PnN'] == channel]['$PnV'].iloc[0]
+            
+            if control_v != exp_v:
+                raise RuntimeError("Voltage differs for channel {0} in tube {1}"
+                                   .format(channel, self.controls[channel]))
+    
         for channel in channels:       
-            tube = fc.FCMeasurement(ID="blank", datafile = self.controls[channel])
+            tube = fc.FCMeasurement(ID=channel, datafile = self.controls[channel])
             
             try:
                 tube.read_meta()
@@ -121,23 +151,44 @@ class BleedthroughPiecewiseOp(HasStrictTraits):
             
             self.voltage[channel] = experiment.metadata[channel]['voltage']
 
+        blank_af = {}
+        for channel in channels:
+            blank_af[channel] = np.median(blank_tube.data[channel])
+
         self.bleedthrough = {}
 
         for from_channel in channels:
             self.bleedthrough[from_channel] = {}
             
-            tube = fc.FCMeasurement(ID="blank", datafile = self.controls[from_channel])
-            range = experiment.metadata[from_channel]['range']
-            knots = np.logspace(1, )
+            tube = fc.FCMeasurement(ID=from_channel, datafile = self.controls[from_channel])
+            data = tube.data.sort(from_channel)
+            
+            # i'm not sure about this -- but it cleans up the plots wonderfully
+            for channel in channels:
+                #data = data[data[channel] > 1]
+                data[channel] = data[channel] - blank_af[channel]
+
+            channel_min = data[from_channel].min()
+            channel_max = data[from_channel].max()
+
+            r = experiment.metadata[from_channel]['range']
+            knots = np.logspace(1, math.log(r, 2), num=self.num_knots, base = 2)
+            knots = [k for k in knots if k > channel_min and k < channel_max]
             
             for to_channel in channels:
-                if channel == to_channel:
+                if from_channel == to_channel:
                     continue
                 
-               
-        
+                self.bleedthrough[from_channel][to_channel] = \
+                    scipy.interpolate.LSQUnivariateSpline(data[from_channel].values,
+                                                          data[to_channel].values,
+                                                          t = knots,
+                                                          k = 1)
+                    
+                # TODO - some sort of validity checking.
+
     def apply(self, old_experiment):
-        """Applies the threshold to an experiment.
+        """Applies the bleedthrough correction to an experiment.
         
         Parameters
         ----------
@@ -146,15 +197,39 @@ class BleedthroughPiecewiseOp(HasStrictTraits):
             
         Returns
         -------
-            a new experiment with the autofluorescence median subtracted from
-            the values in self.blank_file
+            a new experiment with the bleedthrough subtracted out.
         """
         
-        new_experiment = old_experiment.clone()
+        def correct_bleedthrough(row, channels):
+            idx = {channel : idx for idx, channel in enumerate(channels)}
+            
+            def err_channel(x, channel):
+                ret = row[channel] - x[idx[channel]]
+                for from_channel in [c for c in channels if c != channel]:
+                    ret -= self.bleedthrough[from_channel][channel] \
+                                            (x[idx[from_channel]])
+                return ret
+            
+            def err_bld(x):
+                ret = [err_channel(x, channel) for channel in channels]
+                return ret
+            
+            x_0 = row.loc[channels].convert_objects(convert_numeric = True)
+            x = scipy.optimize.root(err_bld, x_0)
+            
+            ret = row.copy()
+            for idx, channel in enumerate(channels):
+                ret[channel] = x.x[idx]
                 
-        for channel in self.autofluorescence.keys():
-            new_experiment[channel] = old_experiment[channel] - \
-                                      self.autofluorescence[channel]
+            return ret
+        
+        channels = self.bleedthrough.keys()
+        old_data = old_experiment.data[channels].copy()
+        new_data = old_data.apply(correct_bleedthrough, axis = 1, args = ([channels]))
+                
+        new_experiment = old_experiment.clone()
+        for channel in channels:
+            new_experiment[channel] = new_data[channel]
 
         return new_experiment
     
@@ -168,7 +243,21 @@ class BleedthroughPiecewiseOp(HasStrictTraits):
             IView : An IView, call plot() to see the diagnostic plots
         """
         
-        return 
+        if set(self.controls.keys()) != set(self.bleedthrough.keys()):
+            raise "Must have both the controls and bleedthrough to plot" 
+ 
+        channels = self.controls.keys()
+        
+        # make sure we can get the control tubes to plot the diagnostic
+        for channel in channels:       
+            tube = fc.FCMeasurement(ID=channel, datafile = self.controls[channel])
+            
+            try:
+                tube.read_meta()
+            except Exception:
+                raise RuntimeError("FCS reader threw an error on tube {0}".format(self.controls[channel]))
+        
+        return BleedthroughPiecewiseDiagnostic(op = self)
         
 @provides(IView)
 class BleedthroughPiecewiseDiagnostic(HasStrictTraits):
@@ -205,7 +294,40 @@ class BleedthroughPiecewiseDiagnostic(HasStrictTraits):
         kwargs.setdefault('antialiased', True)
          
         plt.figure()
-                    
+        
+        channels = self.op.bleedthrough.keys()
+        num_channels = len(channels)
+        
+        for from_idx, from_channel in enumerate(channels):
+            for to_idx, to_channel in enumerate(channels):
+                if from_idx == to_idx:
+                    continue
+
+                tube = fc.FCMeasurement(ID=from_channel, 
+                                        datafile = self.op.controls[from_channel])
+                
+                                
+                plt.subplot(num_channels, 
+                            num_channels, 
+                            from_idx + (to_idx * num_channels) + 1)
+                plt.xscale('log', nonposx='mask')
+                plt.yscale('log', nonposy='mask')
+                plt.xlabel(from_channel)
+                plt.ylabel(to_channel)
+                plt.scatter(tube.data[from_channel],
+                            tube.data[to_channel],
+                            alpha = 0.1,
+                            s = 1,
+                            marker = 'o')
+                
+                spline = self.op.bleedthrough[from_channel][to_channel]
+                xs = np.logspace(-1, math.log(tube.data[from_channel].max(), 10))
+            
+                plt.plot(xs, 
+                         spline(xs), 
+                         'g-', 
+                         lw=3)
+
     def is_valid(self, experiment):
         """Validate this view against an experiment."""
         
