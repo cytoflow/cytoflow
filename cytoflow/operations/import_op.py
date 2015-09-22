4032076ca8483a47360ab47b33b7bc197c34ac5e
Brian Teague <teague@mit.edu>
2015-09-22 19:41:38 -0400

    Improved import docs

--- cytoflow/operations/import_op.py
+++ cytoflow/operations/import_op.py
@@ -12,9 +12,41 @@ import FlowCytometryTools as fc
 class Tube(HasStrictTraits):
     """
     Represents a tube or plate well we want to import.
+    
+    Attributes
+    ----------
+    file : File
+        The file name of the FCS file to import
+        
+    conditions : Dict(Str, Any)
+        A dictionary containing this `Tube`'s experimental conditions.  Keys
+        are condition names, values are condition values.
+    
+    source : Str
+        The sample source, from the $SRC FCS file keyword.  Optional for 
+        interactive use.
+        
+    tube : Str
+        The sample tube, from the TUBE NAME or SMNO FCS keyword.  Optional for
+        interactive use.
+        
+    row : Str
+        If this FCS file was a well from a multi-well plate, `row` contains
+        the well's row.  Optional for interactive use.
+        
+    col : Int
+        If this FCS file was a well from a multi-well plate, 'col' contains the
+        well's column.  Optional for interactive use.
+        
+    Examples
+    --------
+    tube1 = flow.Tube(file = '../../cytoflow/tests/data/Plate01/RFP_Well_A3.fcs',
+                      conditions = {"Dox" : 10.0})
+    tube2 = flow.Tube(file='../../cytoflow/tests/data/Plate01/CFP_Well_A4.fcs',
+                      conditions = {"Dox" : 1.0})
     """
     
-    # name, row and col are optional for interactive use 
+    # source, name, row and col are optional for interactive use 
     # (needed for GUI persistance)
     source = Str
     tube = Str
@@ -35,9 +67,69 @@ class Tube(HasStrictTraits):
 
 @provides(IOperation)
 class ImportOp(HasStrictTraits):
-    '''
-    classdocs
-    '''
+    """
+    An operation for importing data and making an `Experiment`.
+    
+    To use, set the `conditions` dict to a mapping between condition name and
+    NumPy `dtype`.  Useful dtypes include "category", "float", "int", "bool", 
+    and "log". ("log" is not a NumPy `dtype`: instead, it is converted to 
+    `float`, but all plots are displayed on a log scale.)
+    
+    Next, set `tubes` to a list of `Tube`s containing FCS filenames and the
+    corresponding conditions.
+    
+    If you would rather not analyze every single event in every FCS file,
+    set `coarse` to `True` and `coarse_events` to the number of events from
+    each FCS file you want to load.
+    
+    Call `apply()` to load the data.
+    
+    Attributes
+    ----------
+    
+    conditions : Dict(Str, Str)
+        A dictionary mapping condition names (keys) to NumPy `dtype`s (values).
+        Useful `dtype`s include "category", "float", "int", "bool", and "log". 
+        ("log" is not a NumPy `dtype`: instead, it is converted to `float`, 
+        but all plots are displayed on a log scale.)
+        
+    tubes : List(Tube)
+        A list of `Tube` instances, which map FCS files to their corresponding
+        experimental conditions.  Each `Tube` must have a `conditions` dict
+        whose keys match `self.conditions.keys()`.
+        
+    coarse : Bool
+        Do we want to import a random subset of events?  Presumably the analysis
+        will go faster but less precisely; good for interactive exploration.
+        Then, set `coarse = False` and re-run the analysis non-interactively.
+        
+    coarse_events : Int (default = 1000)
+        If `coarse == True`, how many random events to choose from each FCS 
+        file.
+        
+    ignore_v : Bool
+        **CytoFlow** is designed to operate on `Experiment`s containing tubes
+        that were all collected under the instrument settings.  In particular,
+        the same PMV voltages ensure that data can be compared across samples.
+        
+        *Very rarely*, you may need to set up an Experiment with different 
+        voltage settings.  This is likely only to be the case when you are
+        trying to figure out which voltages should be used in future
+        experiments.  If so, set `ignore_v` to `True` to disable the voltage
+        sanity check.  **BE WARNED - THIS WILL BREAK REAL EXPERIMENTS.**
+        
+    Examples
+    --------
+    tube1 = flow.Tube(file = '../../cytoflow/tests/data/Plate01/RFP_Well_A3.fcs',
+                      conditions = {"Dox" : 10.0})
+    tube2 = flow.Tube(file='../../cytoflow/tests/data/Plate01/CFP_Well_A4.fcs',
+                      conditions = {"Dox" : 1.0})
+    
+    import_op = flow.ImportOp(conditions = {"Dox" : "float"},
+                              tubes = [tube1, tube2])
+    
+    ex = import_op.apply()
+    """
 
     id = "edu.mit.synbio.cytoflow.operations.import"
     friendly_id = "Import"
@@ -82,7 +174,6 @@ class ImportOp(HasStrictTraits):
         return True
       
     def apply(self, experiment = None):
-        
         experiment = Experiment()
             
         for condition, dtype in self.conditions.items():
