52a8d7a8b0cc46e249a7820e33852d6e236cc7c9
Brian Teague <teague@mit.edu>
2015-09-25 17:03:33 -0400

    Exclude logicle extension from docs

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ release = '0.1.7'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '*.logicle_ext.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
