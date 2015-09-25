7ba58ede6fa8ba4d9604e98d79d7b3ef9fa8435a
Brian Teague <teague@mit.edu>
2015-09-25 02:19:31 -0400

    pandas req

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['cytoflow.operations.logicle_ext.Logicle',
+MOCK_MODULES = ['cytoflow.operations.logicle_ext.Logicle', 'pandas',
                 'matplotlib', 'matplotlib.pyplot', 'matplotlib.transforms', 'matplotlib.widgets', 'matplotlib.lines', 'matplotlib.patches', 'matplotlib.figure',
                 'traits', 'traits.api']
 
