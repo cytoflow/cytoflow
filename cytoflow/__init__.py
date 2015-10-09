d03414d78673135576ebade2f4320d8533f72b9a
Brian Teague <teague@mit.edu>
2015-10-09 12:46:31 -0400

    RangeOp --> default_view()
    Conflicts:
    	cytoflow/__init__.py

--- cytoflow/__init__.py
+++ cytoflow/__init__.py
@@ -16,7 +16,7 @@ from operations.binning import BinningOp
 from views.histogram import HistogramView
 from views.hexbin import HexbinView
 from views.scatterplot import ScatterplotView
-\from views.stats_1d import Stats1DView
+from views.stats_1d import Stats1DView
 from views.stats_2d import Stats2DView
 from views.bar_chart import BarChartView
 
