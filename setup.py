43410e80b751334ff14edde0d49b0e1a0d14184d
Brian Teague <teague@mit.edu>
2016-01-23 14:55:21 -0500

    License --> GPLv2

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python2.7
+
+# (c) Massachusetts Institute of Technology 2015-2016
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import print_function
 from setuptools import setup, find_packages, Extension
 import io, os, re
@@ -85,13 +102,13 @@ setup(
     keywords = "flow cytometry scipy",
     url = "https://github.com/bpteague/cytoflow", 
     classifiers=[
-                 'Development Status :: 2 - Pre-Alpha',
+                 'Development Status :: 3 - Alpha',
                  'Environment :: Console',
                  'Environment :: MacOS X',
                  'Environment :: Win32 (MS Windows)',
                  'Environment :: X11 Applications :: Qt',
                  'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+                 'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
                  'Natural Language :: English',
                  'Operating System :: MacOS',
                  'Operating System :: Microsoft :: Windows',
