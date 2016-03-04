c039997b872a29cb6f29bd0a3a0a5062a68f1e04
Brian Teague <bpteague@gmail.com>
2016-03-03 22:42:33 -0500

    mock out bottleneck

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['cytoflow.utility.logicle_ext.Logicle']
+MOCK_MODULES = ['cytoflow.utility.logicle_ext.Logicle', 'bottleneck']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
  
