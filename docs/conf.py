d887403fa338ea27261e351faac9f8e1f3fb8b1a
Brian Teague <bpteague@gmail.com>
2016-03-03 22:15:33 -0500

    Update sphinx conf to fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['cytoflow.operations.logicle_ext.Logicle']
+MOCK_MODULES = ['cytoflow.utility.logicle_ext.Logicle']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
  
