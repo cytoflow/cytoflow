0cd675d4d1a7ffa5cd27d06c3dd864c32b0ec295
Brian Teague <bpteague@gmail.com>
2016-03-10 23:55:51 -0500

    test multiprocessing

---
+++ cytoflowgui/test_multi.py
@@ -0,0 +1,76 @@
+'''
+Created on Mar 10, 2016
+
+@author: brian
+'''
+
+from __future__ import print_function
+
+from traits.api import HasTraits, Int, on_trait_change, Instance
+from multiprocessing.managers import BaseManager, NamespaceProxy
+import time
+
+
+class T(HasTraits):
+    i = Int(3)
+    
+    @on_trait_change('i')
+    def pr(self):
+        print("i: {0}".format(self.i))
+        
+class M(HasTraits):
+    t = Instance(T, ())
+
+class TraitsManager(BaseManager):
+    pass
+
+class HasTraitsProxy(NamespaceProxy):
+    # We need to expose the same __dunder__ methods as NamespaceProxy,
+    # in addition to the b method.
+    _exposed_ = ('__getattribute__', '__setattr__', '__delattr__', 'traits')
+    
+    def __getattr__(self, key):
+        print("key {0}".format(key))
+        if key[0] == '_':
+            return object.__getattribute__(self, key)
+        callmethod = object.__getattribute__(self, '_callmethod')
+        
+        if key in callmethod('traits', ()):
+            return callmethod('__getattribute__', (key,))
+        else:
+            return lambda *args, **kwds: callmethod(key, args, kwds)
+    
+    def __setattr__(self, key, value):
+        if key[0] == '_':
+            return object.__setattr__(self, key, value)
+        callmethod = object.__getattribute__(self, '_callmethod')
+        return callmethod('__setattr__', (key, value))
+    def __delattr__(self, key):
+        if key[0] == '_':
+            return object.__delattr__(self, key)
+        callmethod = object.__getattribute__(self, '_callmethod')
+        return callmethod('__delattr__', (key,))
+
+# 
+#     def pr(self, j):
+#         callmethod = object.__getattribute__(self, '_callmethod')
+#         return callmethod('pr', (j, ))
+
+TraitsManager.register('T', T, proxytype = HasTraitsProxy)
+TraitsManager.register('M', M, proxytype = HasTraitsProxy)
+
+if __name__ == '__main__':
+    manager = TraitsManager()
+    manager.start()
+    
+    t = manager.T()
+    
+    print(t.i)
+    t.i = 5
+    print(t.i)
+
+    
+    #print(t.i)
+    #t.i = 5
+    #print(t.i)
+    #t.pr(7)
