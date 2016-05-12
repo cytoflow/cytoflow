1bd117375b26b97c789f027e04c29868eb4d0afb
Brian Teague <teague@mit.edu>
2016-05-12 18:37:30 -0400

    Use the right error for Py2.7

--- pyinstaller/hook-numpy.py
+++ pyinstaller/hook-numpy.py
@@ -10,5 +10,5 @@ for libdir in libdirs:
          logger = logging.getLogger(__name__)
          logger.info("MKL installed as part of numpy, importing that!")
          binaries = [(libdir + "/" + l, '') for l in mkllib]
-   except FileNotFoundError:
+   except OSError:
       pass
