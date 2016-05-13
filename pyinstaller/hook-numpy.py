26b29bb9309b397c397fb55974075a217f590b0d
Brian Teague <teague@mit.edu>
2016-05-13 11:36:52 -0400

    Update numpy hook to find mkl libs on Mac, too

--- pyinstaller/hook-numpy.py
+++ pyinstaller/hook-numpy.py
@@ -5,10 +5,11 @@ from os import listdir
 libdirs = [compat.base_prefix + "/lib", compat.base_prefix + "/Library/bin"]
 for libdir in libdirs:
    try:
-      mkllib = [x for x in listdir(libdir) if x.startswith('mkl_')]
+      mkllib = [x for x in listdir(libdir) if x.startswith('mkl_')
+                                           or x.startswith('libmkl_')]
       if mkllib <> []: 
          logger = logging.getLogger(__name__)
-         logger.info("MKL installed as part of numpy, importing that!")
+         logger.info("Adding the MKL libraries to binaries.")
          binaries = [(libdir + "/" + l, '') for l in mkllib]
    except OSError:
       pass
