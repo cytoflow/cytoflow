4f15b70e467ec9a45b857b4ce745a6feaf6382d5
Brian Teague <bpteague@gmail.com>
2016-05-15 11:18:38 -0400

    1D and 2D KDEs

---
+++ cytoflow/views/kde_1d.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python2.7
+
+# (c) Massachusetts Institute of Technology 2015-2016
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import division, absolute_import
+
+from traits.api import HasStrictTraits, Str, provides
+import matplotlib.pyplot as plt
+
+import numpy as np
+import seaborn as sns
+import statsmodels.nonparametric.api as smnp
+
+import cytoflow.utility as util
+from .i_view import IView
+
+@provides(IView)
+class Kde1DView(HasStrictTraits):
+    """Plots a one-channel kernel density estimate
+    
+    Attributes
+    ----------
+    name : Str
+        The view's name (for serialization, UI etc.)
+    
+    channel : Str
+        the name of the channel we're plotting
+    
+    xfacet : Str 
+        the conditioning variable for multiple plots (horizontal)
+    
+    yfacet : Str
+        the conditioning variable for multiple plots (vertical)
+    
+    huefacet : Str
+        the conditioning variable for multiple plots (color)
+        
+    subset : Str
+        a string passed to pandas.DataFrame.query() to subset the data before 
+        we plot it.
+        
+        .. note: Should this be a param instead?
+        
+    Examples
+    --------
+    >>> kde = flow.Kde1DView()
+    >>> kde.name = "Kernel Density 1D"
+    >>> kde.channel = 'Y2-A'
+    >>> kde.xfacet = 'Dox'
+    >>> kde.yfacet = 'Y2-A+'
+    >>> kde.plot(ex)
+    """
+    
+    # traits   
+    id = "edu.mit.synbio.cytoflow.view.kde1d"
+    friendly_id = "1D Kernel Density" 
+    
+    name = Str
+    channel = Str
+    scale = util.ScaleEnum
+    xfacet = Str
+    yfacet = Str
+    huefacet = Str
+    subset = Str
+    
+    def plot(self, experiment, **kwargs):
+        """Plot a faceted histogram view of a channel"""
+        
+        if not experiment:
+            raise util.CytoflowViewError("No experiment specified")
+        
+        if not self.channel:
+            raise util.CytoflowViewError("Must specify a channel")
+        
+        if self.channel not in experiment.data:
+            raise util.CytoflowViewError("Channel {0} not in the experiment"
+                                    .format(self.channel))
+        
+        if self.xfacet and self.xfacet not in experiment.conditions:
+            raise util.CytoflowViewError("X facet {0} not in the experiment"
+                                    .format(self.xfacet))
+        
+        if self.yfacet and self.yfacet not in experiment.conditions:
+            raise util.CytoflowViewError("Y facet {0} not in the experiment"
+                                    .format(self.yfacet))
+        
+        if self.huefacet and self.huefacet not in experiment.conditions:
+            raise util.CytoflowViewError("Hue facet {0} not in the experiment"
+                                    .format(self.huefacet))
+
+        if self.subset:
+            try:
+                data = experiment.query(self.subset)
+            except:
+                raise util.CytoflowViewError("Subset string '{0}' isn't valid"
+                                        .format(self.subset))
+                
+            if len(data.index) == 0:
+                raise util.CytoflowViewError("Subset string '{0}' returned no events"
+                                        .format(self.subset))
+        else:
+            data = experiment.data
+            
+
+        #print scaled_data
+        
+        kwargs.setdefault('shade', True)
+        kwargs['label'] = self.name
+        
+        g = sns.FacetGrid(data, 
+                          size = 6,
+                          aspect = 1.5,
+                          col = (self.xfacet if self.xfacet else None),
+                          row = (self.yfacet if self.yfacet else None),
+                          hue = (self.huefacet if self.huefacet else None),
+                          col_order = (np.sort(data[self.xfacet].unique()) if self.xfacet else None),
+                          row_order = (np.sort(data[self.yfacet].unique()) if self.yfacet else None),
+                          hue_order = (np.sort(data[self.huefacet].unique()) if self.huefacet else None),
+                          legend_out = False,
+                          sharex = False,
+                          sharey = False)
+        
+        # get the scale     
+        kwargs['scale'] = scale = util.scale_factory(self.scale, experiment, self.channel)
+        
+        # set the scale for each set of axes; can't just call plt.xscale() 
+        for ax in g.axes.flatten():
+            ax.set_xscale(self.scale, **scale.mpl_params)  
+                  
+        g.map(_univariate_kdeplot, self.channel, **kwargs)
+        g.add_legend()
+
+# yoinked from seaborn/distributions.py, with modifications for scaling.
+
+def _univariate_kdeplot(data, scale=None, shade=False, kernel="gau",
+        bw="scott", gridsize=100, cut=3, clip=None, legend=True,
+        cumulative=False, shade_lowest=True, ax=None, **kwargs):
+    
+    if ax is None:
+        ax = plt.gca()
+        
+    if clip is None:
+        clip = (-np.inf, np.inf)
+
+    scaled_data = scale(data)
+    
+    # mask out the data that's not in the scale domain
+    scaled_data = scaled_data[~np.isnan(scaled_data)]  
+    
+    # Calculate the KDE
+    fft = (kernel == "gau")
+    kde = smnp.KDEUnivariate(scaled_data)
+    kde.fit(kernel, bw, fft, gridsize=gridsize, cut=cut, clip=clip)
+
+    x, y = scale.inverse(kde.support), kde.density
+
+    # Make sure the density is nonnegative
+    y = np.amax(np.c_[np.zeros_like(y), y], axis=1)
+
+    # Check if a label was specified in the call
+    label = kwargs.pop("label", None)
+
+    color = kwargs.pop("color", None)
+
+    # Draw the KDE plot and, optionally, shade
+    ax.plot(x, y, color=color, label=label, **kwargs)
+    alpha = kwargs.get("alpha", 0.25)
+    if shade:
+        ax.fill_between(x, 1e-12, y, facecolor=color, alpha=alpha)
+
+    return ax
---
+++ cytoflow/views/kde_2d.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python2.7
+
+# (c) Massachusetts Institute of Technology 2015-2016
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Created on Apr 19, 2015
+
+@author: brian
+"""
+
+from six import string_types
+
+from traits.api import HasStrictTraits, provides, Str
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import seaborn as sns
+import numpy as np
+import statsmodels.nonparametric.api as smnp
+
+import cytoflow.utility as util
+from .i_view import IView
+
+@provides(IView)
+class Kde2DView(HasStrictTraits):
+    """
+    Plots a 2-d kernel density estimate.  
+    
+    Attributes
+    ----------
+    
+    name : Str
+        The name of the plot, for visualization (and the plot title)
+        
+    xchannel : Str
+        The channel to plot on the X axis
+        
+    xscale : Enum("linear", "log", "logicle") (default = "linear")
+        What scale to use on the X axis
+        
+    ychannel : Str
+        The channel to plot on the Y axis
+        
+    yscale : Enum("linear", "log", "logicle") (default = "linear")
+        The scale to use on the Y axis
+        
+    xfacet : Str
+        The conditioning variable for multiple plots (horizontal)
+        
+    yfacet = Str
+        The conditioning variable for multiple plots (vertical)
+        
+    huefacet = Str
+        The conditioning variable for multiple plots (color)
+
+    subset = Str
+        A string passed to pandas.DataFrame.query() to subset the data before
+        we plot it.
+        
+        .. note: should this be a param instead?
+    """
+    
+    id = 'edu.mit.synbio.cytoflow.view.kde2d'
+    friend_id = "2D Kernel Density Estimate"
+    
+    name = Str
+    xchannel = Str
+    xscale = util.ScaleEnum
+    ychannel = Str
+    yscale = util.ScaleEnum
+    xfacet = Str
+    yfacet = Str
+    huefacet = Str
+    subset = Str
+    
+    def plot(self, experiment, **kwargs):
+        """Plot a faceted 2d kernel density estimate"""
+        
+        if not experiment:
+            raise util.CytoflowViewError("No experiment specified")
+
+        if not self.xchannel:
+            raise util.CytoflowViewError("X channel not specified")
+        
+        if self.xchannel not in experiment.data:
+            raise util.CytoflowViewError("X channel {0} not in the experiment"
+                                    .format(self.xchannel))
+            
+        if not self.ychannel:
+            raise util.CytoflowViewError("Y channel not specified")
+        
+        if self.ychannel not in experiment.data:
+            raise util.CytoflowViewError("Y channel {0} not in the experiment"
+                                         .format(self.ychannel))
+        
+        if self.xfacet and self.xfacet not in experiment.conditions:
+            raise util.CytoflowViewError("X facet {0} not in the experiment"
+                                         .format(self.xfacet))
+        
+        if self.yfacet and self.yfacet not in experiment.conditions:
+            raise util.CytoflowViewError("Y facet {0} not in the experiment"
+                                         .format(self.yfacet))
+        
+        if self.huefacet and self.huefacet not in experiment.metadata:
+            raise util.CytoflowViewError("Hue facet {0} not in the experiment"
+                                         .format(self.huefacet))
+        
+        if self.subset:
+            try:
+                data = experiment.query(self.subset)
+            except:
+                raise util.CytoflowViewError("Subset string '{0}' isn't valid"
+                                        .format(self.subset))
+                            
+            if len(data.index) == 0:
+                raise util.CytoflowViewError("Subset string '{0}' returned no events"
+                                        .format(self.subset))
+        else:
+            data = experiment.data
+        
+        kwargs.setdefault('shade', False)
+        kwargs.setdefault('min_alpha', 0.2)
+        kwargs.setdefault('max_alpha', 0.9)
+        kwargs.setdefault('n_levels', 10)
+
+        g = sns.FacetGrid(data, 
+                          size = 6,
+                          aspect = 1.5,
+                          col = (self.xfacet if self.xfacet else None),
+                          row = (self.yfacet if self.yfacet else None),
+                          hue = (self.huefacet if self.huefacet else None),
+                          col_order = (np.sort(data[self.xfacet].unique()) if self.xfacet else None),
+                          row_order = (np.sort(data[self.yfacet].unique()) if self.yfacet else None),
+                          hue_order = (np.sort(data[self.huefacet].unique()) if self.huefacet else None),
+                          legend_out = False,
+                          sharex = False,
+                          sharey = False)
+        
+        xscale = util.scale_factory(self.xscale, experiment, self.xchannel)
+        yscale = util.scale_factory(self.yscale, experiment, self.ychannel)
+        
+        for ax in g.axes.flatten():
+            ax.set_xscale(self.xscale, **xscale.mpl_params)
+            ax.set_yscale(self.yscale, **yscale.mpl_params)
+            
+        kwargs['xscale'] = xscale
+        kwargs['yscale'] = yscale
+
+        g.map(_bivariate_kdeplot, self.xchannel, self.ychannel, **kwargs)
+        g.add_legend()
+        
+# yoinked from seaborn/distributions.py, with modifications for scaling.
+def _bivariate_kdeplot(x, y, xscale=None, yscale=None, shade=False, kernel="gau",
+                       bw="scott", gridsize=100, cut=3, clip=None, legend=True, **kwargs):
+    
+    ax = plt.gca()
+    
+    # Determine the clipping
+    clip = [(-np.inf, np.inf), (-np.inf, np.inf)]
+        
+    x = xscale(x)
+    y = yscale(y)
+
+    # Compute a bivariate kde using statsmodels.
+    if isinstance(bw, string_types):
+        bw_func = getattr(smnp.bandwidths, "bw_" + bw)
+        x_bw = bw_func(x)
+        y_bw = bw_func(y)
+        bw = [x_bw, y_bw]
+    elif np.isscalar(bw):
+        bw = [bw, bw]
+
+    kde = smnp.KDEMultivariate([x, y], "cc", bw)
+    x_support = _kde_support(x, kde.bw[0], gridsize, cut, clip[0])
+    y_support = _kde_support(y, kde.bw[1], gridsize, cut, clip[1])
+    xx, yy = np.meshgrid(x_support, y_support)
+    z = kde.pdf([xx.ravel(), yy.ravel()]).reshape(xx.shape)
+
+    # Plot the contours
+    n_levels = kwargs.pop("n_levels", 10)
+    color = kwargs.pop("color")
+    kwargs['colors'] = (color, )
+    
+    x_support = xscale.inverse(x_support)
+    y_support = yscale.inverse(y_support)
+    xx, yy = np.meshgrid(x_support, y_support)    
+    
+    contour_func = ax.contourf if shade else ax.contour
+    cset = contour_func(xx, yy, z, n_levels, **kwargs)
+    num_collections = len(cset.collections)
+    
+    min_alpha = kwargs.pop("min_alpha", 0.2)
+    if shade:
+        min_alpha = 0
+        
+    max_alpha = kwargs.pop("max_alpha", 0.9)
+    
+    alpha = np.linspace(min_alpha, max_alpha, num = num_collections)
+    for el in range(num_collections):
+        cset.collections[el].set_alpha(alpha[el])
+
+    # Label the axes
+    if hasattr(x, "name") and legend:
+        ax.set_xlabel(x.name)
+    if hasattr(y, "name") and legend:
+        ax.set_ylabel(y.name)
+
+    return ax        
+
+def _kde_support(data, bw, gridsize, cut, clip):
+    """Establish support for a kernel density estimate."""
+    support_min = max(data.min() - bw * cut, clip[0])
+    support_max = min(data.max() + bw * cut, clip[1])
+    return np.linspace(support_min, support_max, gridsize)
+
+if __name__ == '__main__':
+    import cytoflow as flow
+    tube1 = flow.Tube(file = '../../cytoflow/tests/data/Plate01/RFP_Well_A3.fcs',
+                      conditions = {"Dox" : 10.0})
+    
+    tube2 = flow.Tube(file = '../../cytoflow/tests/data/Plate01/CFP_Well_A4.fcs',
+                      conditions = {"Dox" : 1.0})                      
+
+    ex = flow.ImportOp(conditions = {"Dox" : "float"}, tubes = [tube1, tube2])
+    
+    thresh = flow.ThresholdOp()
+    thresh.name = "Y2-A+"
+    thresh.channel = 'Y2-A'
+    thresh.threshold = 200.0
+
+    ex2 = thresh.apply(ex)
+    
+    scatter = flow.ScatterplotView()
+    scatter.name = "Scatter"
+    scatter.xchannel = "FSC-A"
+    scatter.ychannel = "SSC-A"
+    scatter.xscale = "logicle"
+    scatter.yscale = "logicle"
+    scatter.huefacet = 'Dox'
+    
+    plt.ioff()
+    scatter.plot(ex2)
+    plt.show()
+    
+
+# modified from http://stackoverflow.com/questions/16834861/create-own-colormap-using-matplotlib-and-plot-color-scale
+def _make_colormap(seq):
+    """Return a LinearSegmentedColormap
+    seq: a sequence of floats and RGB-tuples. The floats should be increasing
+    and in the interval (0,1).
+    """
+    seq = [(None,) * 3, 0.0] + list(seq) + [1.0, (None,) * 3]
+    cdict = {'red': [], 'green': [], 'blue': []}
+    for i, item in enumerate(seq):
+        if isinstance(item, float):
+            r1, g1, b1 = seq[i - 1]
+            r2, g2, b2 = seq[i + 1]
+            cdict['red'].append([item, r1, r2])
+            cdict['green'].append([item, g1, g2])
+            cdict['blue'].append([item, b1, b2])
+    return mcolors.LinearSegmentedColormap('CustomMap', cdict)
