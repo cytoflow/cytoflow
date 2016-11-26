ee09c7178adb6dea55ab496c5d879495e8848153
Brian Teague <bpteague@gmail.com>
2016-11-25 22:00:37 -0500

    squelch complaining Removed traits

--- cytoflow/utility/custom_traits.py
+++ cytoflow/utility/custom_traits.py
@@ -23,6 +23,7 @@ Created on Oct 12, 2015
 
 from __future__ import absolute_import
 from warnings import warn
+import inspect
 
 from traits.api import BaseCInt, BaseCFloat, BaseEnum, TraitType
 from . import scale
@@ -76,30 +77,49 @@ class ScaleEnum(BaseEnum):
 class Removed(TraitType):
     def __init__(self, **metadata):
         metadata.setdefault('err_string', 'Trait {} has been removed')
+        metadata.setdefault('transient', True)
         super(Removed, self).__init__(**metadata)
     
     def get(self, obj, name):
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe)
+        if calframe[1][3] == "copy_traits":
+            return
+        
         if self.warning:
             warn(self.err_string.format(name), CytoflowWarning)
         else:
             raise CytoflowError(self.err_string.format(name))
     
     def set(self, obj, name, value):
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 2)
+        if calframe[1][3] == "copy_traits":
+            return
+        
         if self.warning:
             warn(self.err_string.format(name), CytoflowWarning)
-        raise CytoflowError(self.err_string.format(name))
+        else:
+            raise CytoflowError(self.err_string.format(name))
     
 class Deprecated(TraitType):  
     def __init__(self, **metadata):
         metadata.setdefault('err_string', 'Trait {} is deprecated; please use {}')
+        metadata.setdefault('transient', True)
         super(Deprecated, self).__init__(**metadata)
       
     def get(self, obj, name):
-        warn(self.err_string.format(name, self.new), CytoflowWarning)
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe)
+        if calframe[1][3] != "copy_traits":
+            warn(self.err_string.format(name, self.new), CytoflowWarning)
         return getattr(obj, self.new)
     
     def set(self, obj, name, value):
-        warn(self.err_string.format(name, self.new), CytoflowWarning)
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe)
+        if calframe[1][3] != "copy_traits":
+            warn(self.err_string.format(name, self.new), CytoflowWarning)
         setattr(obj, self.new, value)
         
     
