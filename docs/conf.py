13d86168184b5b8c2178c0df8fe987db64175e6f
Brian Teague <teague@mit.edu>
2017-08-13 15:51:17 -0400

    exclude logicle

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ release = cytoflow.__version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '*.logicle_ext.rst']
+exclude_patterns = ['_build', '*.logicle_ext*.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
