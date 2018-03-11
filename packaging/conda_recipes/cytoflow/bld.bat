fdaad719191e8dd62582e9f83365dd1a70be1d5d
Brian Teague <bpteague@gmail.com>
2018-03-10 23:06:02 -0500

    conda package recipes

---
+++ packaging/conda_recipes/cytoflow/bld.bat
@@ -0,0 +1,8 @@
+"%PYTHON%" setup.py install
+if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: https://conda.io/docs/user-guide/tasks/build-packages/index.html
+:: for a list of environment variables that are set during the build process.
