720d7c7b0eadaf2aa8ff0e1d752388ff261711a7
Brian Teague <bpteague@gmail.com>
2018-03-13 20:13:38 -0400

    fix appveyor bintray stuff. try travis again.

--- packaging/move_conda_package.py
+++ packaging/move_conda_package.py
@@ -1,18 +1,7 @@
-'''
-From https://github.com/rmcgibbo/python-appveyor-conda-example/blob/master/continuous-integration/move-conda-package.py
-'''
-
-import sys
-import os
-import yaml
-import glob
 import shutil
-from conda_build.config import config  # @UnresolvedImport
+from conda_build.api import get_output_file_paths  # @UnresolvedImport
 
-with open(os.path.join(sys.argv[1], 'meta.yaml')) as f:
-    name = yaml.load(f)['package']['name']
+binary_package_paths = get_output_file_paths("packaging/conda_recipes/cytoflow")
 
-binary_package_glob = os.path.join(config.bldpkgs_dir, '{0}*.tar.bz2'.format(name))
-binary_package = glob.glob(binary_package_glob)[0]
-
-shutil.move(binary_package, '.')
+for p in binary_package_paths:
+    shutil.move(p, 'dist/')
