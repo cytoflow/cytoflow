9bddadd3e51435fc163b643eb36d5b0ee388bcfd
Brian Teague <bpteague@gmail.com>
2018-04-20 23:05:51 -0400

    set a better default window size

--- cytoflowgui/cytoflow_application.py
+++ cytoflowgui/cytoflow_application.py
@@ -129,16 +129,6 @@ class CytoflowApplication(TasksApplication):
     def stop(self):
         super().stop()
         self.model.shutdown_remote_process()
-#         
-#         
-#     def exit(self, force=False):
-# 
-#         ret = TasksApplication.exit(self, force=force)
-#         
-#         if ret:
-#             self.model.shutdown_remote_process()
-#             
-#         return ret
         
 
     preferences_helper = Instance(CytoflowPreferences)
@@ -163,6 +153,9 @@ class CytoflowApplication(TasksApplication):
                     restored_state = pickle.load(f)
                 if state.version == restored_state.version:
                     state = restored_state
+                    
+                    # make sure the active task is the main window
+                    state.previous_window_layouts[0].active_task = 'edu.mit.synbio.cytoflowgui.flow_task'
                 else:
                     logger.warn('Discarding outdated application layout')
             except:
@@ -174,12 +167,9 @@ class CytoflowApplication(TasksApplication):
      
     def _save_state(self):
         """
-        Saves the application state -- ONLY IF THE CYTOFLOW TASK IS ACTIVE
+        Saves the application state
         
         """
-        if self.active_window.active_task.id != "edu.mit.synbio.cytoflowgui.flow_task":
-            logger.info("Not saving application layout from TASBE task")
-            return
         
         # Grab the current window layouts.
         window_layouts = [w.get_window_layout() for w in self.windows]
@@ -200,12 +190,13 @@ class CytoflowApplication(TasksApplication):
         tasks = [ factory.id for factory in self.task_factories ]
         return [ TaskWindowLayout(*tasks,
                                   active_task = active_task,
-                                  size = (800, 600)) ]
+                                  size = (1280, 800)) ]
 
     def _preferences_helper_default(self):
         return CytoflowPreferences(preferences = self.preferences)
 
     #### Trait property getter/setters ########################################
- 
+
+    # should we restore the saved layout? 
     def _get_always_use_default_layout(self):
         return self.preferences_helper.always_use_default_layout
