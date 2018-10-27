65da2016924d99f25b2f03c2e44ad1ca39c7f513
Brian Teague <teague@mit.edu>
2018-10-26 22:02:08 -0500

    packaging: don't make qt.conf a resource

--- packaging/rthook_qtconf.py
+++ packaging/rthook_qtconf.py
@@ -24,17 +24,3 @@ if sys.platform.startswith('win'):
 with open(qt_conf_path, 'w') as qt_conf:
     qt_conf.write('[Paths]\nPrefix = {}\n'.format(qt_path))
 
-with open(qrc_path, 'w') as qrc:
-    qrc.write("""
-<!DOCTYPE RCC>
-<RCC version="1.0">
-<qresource prefix="/qt/etc">
-    <file alias="qt.conf">qt.conf</file>
-</qresource>
-</RCC> """)
-
-import PyQt5.pyrcc_main
-PyQt5.pyrcc_main.processResourceFile([qrc_path], qrc_py_path, False)
-
-import qt_conf
-
