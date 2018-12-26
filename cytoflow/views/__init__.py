b91f3c3833cb795708337946ac2afc8bf1613f5a
Brian Teague <bpteague@gmail.com>
2018-12-26 16:35:26 -0500

    views: set a few more defaults for jupyter notebook

--- cytoflow/views/__init__.py
+++ cytoflow/views/__init__.py
@@ -17,7 +17,17 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 import seaborn as sns
+
+# set seaborn defaults.  this is mostly for the Jupyter notebook;
+# these settings can be overridden in the GUI
+sns.set(context = "notebook", style = "whitegrid")
+
+# figure.figsize interacts strangely with the jupyter notebook.
+# which is to say, i'm not sure why this does what it does, but
+# it fixes axis label font sizes.
+mpl.rc('figure', figsize = [8.0, 5.5])
 
 # the default marker scales are tiny!  make them less tiny
 mpl.rc('legend', markerscale = 5)
