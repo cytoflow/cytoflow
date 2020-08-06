3cb18d4a3d69dbcc68d3adc96628104cfc037146
Brian Teague <bpteague@gmail.com>
2020-08-05 22:08:45 -0500

    monkey-patch nose2 mp so it can run gui tests with multiprocessing.

--- package/nose_setup.py
+++ package/nose_setup.py
@@ -3,16 +3,35 @@ Created on Aug 5, 2020
 
 @author: brian
 '''
-import logging
-import os
+import os, logging, multiprocessing
 
 from nose2.events import Plugin
+from nose2.plugins.mp import MultiProcess, procserver
 
-log = logging.getLogger('nose2.plugins.helloworld')
+log = logging.getLogger('package.nose_setup')
 
-class HelloWorld(Plugin):
-    configSection = 'helloworld'
-    commandLineSwitch = (None, 'hello-world', 'Say hello!')
+def _startProcs(self, test_count):
+    # Create session export
+    session_export = self._exportSession()
+    procs = []
+    count = min(test_count, self.procs)
+    log.debug("Creating %i worker processes", count)
+    for i in range(0, count):
+        parent_conn, child_conn = self._prepConns()
+        proc = multiprocessing.Process(
+            target=procserver, args=(session_export, child_conn))
+        proc.start()
+        parent_conn = self._acceptConns(parent_conn)
+        procs.append((proc, parent_conn))
+    return procs
+
+class NoseSetup(Plugin):
+    configSection = 'nose_setup'
 
     def startTestRun(self, event):
-        log.info('Hello pluginized world!')
+        log.warning('Loading customized nose2 configuration')
+        
+        MultiProcess._startProcs = _startProcs
+        os.environ['OMP_NUM_THREADS'] = '1'
+
+        
