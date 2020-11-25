3c9d842f33bb50876176650bbe17f545debd9a11
Brian Teague <bpteague@gmail.com>
2020-11-24 21:22:35 -0600

    another cmap update

--- cytoflow/operations/flowpeaks.py
+++ cytoflow/operations/flowpeaks.py
@@ -36,6 +36,8 @@ import scipy.ndimage
 
 import pandas as pd
 
+import copy
+
 from cytoflow.views import IView, HistogramView, ScatterplotView
 import cytoflow.utility as util
 
@@ -1077,15 +1079,18 @@ class FlowPeaks2DDensityView(By2DView, AnnotatingView, NullView):
         yscale = kwargs['scale'][self.ychannel]
         ylim = kwargs['lim'][self.ychannel]
         
+        # can't modify colormaps in place
+        cmap = copy.copy(kwargs['cmap'])
+        
         under_color = kwargs.pop('under_color', None)
         if under_color is not None:
-            kwargs['cmap'].set_under(color = under_color)
+            cmap.set_under(color = under_color)
         else:
-            kwargs['cmap'].set_under(color = kwargs['cmap'](0.0))
+            cmap.set_under(color = cmap(0.0))
 
         bad_color = kwargs.pop('bad_color', None)
         if bad_color is not None:
-            kwargs['cmap'].set_bad(color = kwargs['cmap'](0.0))
+            cmap.set_bad(color = cmap(0.0))
         
         gridsize = kwargs.pop('gridsize', 50)
         xbins = xscale.inverse(np.linspace(xscale(xlim[0]), xscale(xlim[1]), gridsize))
