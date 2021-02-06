53c09f3137f9b862d79ab08dc5ead126d0d400a8
Brian Teague <bpteague@gmail.com>
2021-02-06 16:32:48 -0600

    update copyright notice

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3.4
 # coding: latin-1
 
-# (c) Massachusetts Institute of Technology 2015-2016
+# (c) Massachusetts Institute of Technology 2015-2018
+# (c) Brian Teague 2018-2021
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
