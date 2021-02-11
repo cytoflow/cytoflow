57d85c14a9699947e261fdd8b3709bdbf5f0fcd0
Brian Teague <bpteague@gmail.com>
2021-02-10 18:13:49 -0600

    gui: use a comparison operator, not identity

--- cytoflowgui/view_pane.py
+++ cytoflowgui/view_pane.py
@@ -124,7 +124,7 @@ class ViewDockPane(TraitsDockPane):
         if new_view_id:
             self._actions[new_view_id] = self._default_action 
             
-        self._default_action.visible = (new_view_id is not "")
+        self._default_action.visible = (new_view_id != "")
             
     @on_trait_change('selected_view')
     def _selected_view_changed(self, view_id):         
