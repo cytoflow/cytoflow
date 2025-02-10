7fce338e7f450369944185e36ea2394f0f7f003f
Brian Teague <bpteague@gmail.com>
2025-02-09 19:04:47 -0600

    build: collect all of pyface for pyinstaller

--- package/hooks/hook-pyface.py
+++ package/hooks/hook-pyface.py
@@ -3,5 +3,7 @@ from PyInstaller.utils.hooks import (copy_metadata, collect_submodules,
 datas = copy_metadata('pyface') + collect_data_files('pyface')
 
 hiddenimports = collect_submodules('pyface.ui.qt') + \
-                collect_submodules('pyface.ui.qt.action')
+                collect_submodules('pyface.ui.qt.action') + \
+                collect_submodules('pyface.timer') + \
+                collect_submodules('pyface.tasks')
 
