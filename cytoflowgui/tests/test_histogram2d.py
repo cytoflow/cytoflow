512d5a1ad3dd9f5fac3eb25206f1a9aa9ae03b2f
Brian Teague <bpteague@gmail.com>
2025-09-20 19:39:26 -0500

    tests: fix test_histogram2d syntax error

--- cytoflowgui/tests/test_histogram2d.py
+++ cytoflowgui/tests/test_histogram2d.py
@@ -104,7 +104,7 @@ class TestHistogram2D(ImportedDataTest, Base2DViewTest):
             for view in wi.views:
                 code = code + view.get_notebook_code(i)
            
-        exec(globals(), locals()) # smoke test
+        exec(code, globals(), locals()) # smoke test
            
 if __name__ == "__main__":
 #     import sys;sys.argv = ['', 'TestHistogram2D.testSerialize']
