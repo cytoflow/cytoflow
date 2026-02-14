20fa31beb6b2563f621925dbd4c6a2fa57b73a15
Brian Teague <bpteague@gmail.com>
2026-02-13 22:21:36 -0600

    qt6: use pyside6 instead of pyqt6

--- cytoflowgui/__init__.py
+++ cytoflowgui/__init__.py
@@ -24,13 +24,4 @@ cytoflowgui
 The `cytoflowgui` package contains the Qt GUI for `cytoflow`.
 """
 
-# from PyQt5 import sip 
-
-# sip.setapi(u'QDate', 2)
-# sip.setapi(u'QDateTime', 2)
-# sip.setapi(u'QString', 2)
-# sip.setapi(u'QTextStream', 2)
-# sip.setapi(u'QTime', 2)
-# sip.setapi(u'QUrl', 2)
-# sip.setapi(u'QVariant', 2)
 
--- package/hooks/rthook_qtapi.py
+++ package/hooks/rthook_qtapi.py
@@ -1,6 +1,6 @@
 import os, sys
 
-os.environ["QT_API"] = "pyqt5"
+os.environ["QT_API"] = "pyside6"
 
 from traits.etsconfig.api import ETSConfig
 ETSConfig.toolkit = 'qt'
